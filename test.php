ca94df5391a13c52a0e2c2661fef8bf17eba8482
max1303 <max1303@rambler.ru>
2019-10-06 21:18:04 +0300

    06.10.2019

--- test.php
+++ test.php
@@ -2,52 +2,42 @@
 <html>
 
 <head>
-<title>Заголовок страницы</title>
-<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="http://localhost/test.ru/css/style.css">
+	<title>Заголовок страницы</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="http://localhost/test.ru/css/style.css">
 </head>
 
 <body>
 
 
 <?php 
-// ПРИМЕР ФАЙЛА. 
-$filename = 'img/1.jpg'; 
+error_reporting(E_ALL);
 
-// ПРОВЕРКА НА ИЗОБРАЖЕНИЕ
-$size = getimagesize($filename); // если это изображение и у него определён размер, то продолжаем
-if ($size){
+$file = 'http://laprbass.com/RAY_EE_images/headshot.png';
+$file = 'http://laprbass.com/RAY_EE_images/headshot.jpg';
+$file = 'http://laprbass.com/RAY_EE_images/headshot.gif';
 
-// ОПРЕДЕЛЯЕМ МАКСИМАЛЬНЫЕ ШИРИНУ И ВЫСОТУ ИЗОБРАЖЕНИЯ
-$width = 200; 
-$height = 200; 
+$what = getimagesize($file);
 
-// РАБОТАЕМ КОРРЕКТНО. ОПРЕДЕЛЯЕМ ТИП
-header("Content-type: {$size['mime']}");
-
-// ПОЛУЧАЕМ НОВЫЕ РАЗМЕРЫ
-list($width_orig, $height_orig) = getimagesize($filename); 
-
-if ($width && ($width_orig < $height_orig)) { 
-    $width = ($height / $height_orig) * $width_orig; 
-} else { 
-    $height = ($width / $width_orig) * $height_orig; 
-} 
-
-// ПЕРЕСОХРАНЯЕМ ИЗОБРАЖЕНИЕ
-$image_p = imagecreatetruecolor($width, $height); 
-$image = imagecreatefromjpeg($filename); 
-imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig); 
-
-// СОЗДАЁМ
-imagejpeg($image_p, null, 100); 
-
-// УДАЛЯЕМ ИСХОДНИК - НУЖЕН АДРЕС. НАПРИМЕР, images/img001.jpg
-unlink ($filename);
-
-} else {
-exit("Загружаемый файл не изображение...");
+switch(strtolower($what['mime']))
+{
+    case 'image/png':
+        $img = imagecreatefrompng($file);
+        break;
+    case 'image/jpeg':
+        $img = imagecreatefromjpeg($file);
+        break;
+    case 'image/gif':
+        $img = imagecreatefromgif($file);
+        break;
+    default: die();
 }
+$new = imagecreatetruecolor($what[0],$what[1]);
+imagecopy($new,$img,0,0,0,0,$what[0],$what[1]);
+
+header('Content-Type: image/jpeg');
+imagejpeg($new);
+imagedestroy($new);
 ?>
 
 
