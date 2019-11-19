13da9210c4b586919a2935b7473a306509a7e151
max1303 <max1303@rambler.ru>
2019-11-19 21:56:08 +0300

    2019.11.19

---
+++ array_test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Заказы</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="shortcut icon" href="img/favicon.png" type="image/png">
+	<meta name='viewport' content='width=device-width,initial-scale=1'/>
+	<meta content='true' name='HandheldFriendly'/>
+	<meta content='width' name='MobileOptimized'/>
+	<meta content='yes' name='apple-mobile-web-app-capable'/>	
+</head>
+
+<body>
+
+<?
+
+
+
+
+
+
+?>
+
+</body>
+</html>
